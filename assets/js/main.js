899e65775902f806edc3e6e30fb8e46a1754f361
Ivan Toselli <ivan.toselli31@gmail.com>
2022-03-13 16:16:10 +0100

    Stp 1: Exe complete

---
+++ assets/js/main.js
@@ -0,0 +1,445 @@
+// Dichiarazione variabili 0-9
+let num0Var = document.getElementById("num0") 
+
+let num1Var = document.getElementById("num1")
+
+let num2Var = document.getElementById("num2")
+
+let num3Var = document.getElementById("num3")
+
+let num4Var = document.getElementById("num4")
+
+let num5Var = document.getElementById("num5")
+
+let num6Var = document.getElementById("num6")
+
+let num7Var = document.getElementById("num7")
+
+let num8Var = document.getElementById("num8")
+
+let num9Var = document.getElementById("num9")
+
+let i; //Variabile Contatore
+
+// Dichiarazione variabili 10-19
+let num0DVar = document.getElementById("num0d") 
+
+let num1DVar = document.getElementById("num1d")
+
+let num2DVar = document.getElementById("num2d")
+
+let num3DVar = document.getElementById("num3d")
+
+let num4DVar = document.getElementById("num4d")
+
+let num5DVar = document.getElementById("num5d")
+
+let num6DVar = document.getElementById("num6d")
+
+let num7DVar = document.getElementById("num7d")
+
+let num8DVar = document.getElementById("num8d")
+
+let num9DVar = document.getElementById("num9d")
+
+// Dichiarazione variabili 20-29
+let num0VVar = document.getElementById("num0v") 
+
+let num1VVar = document.getElementById("num1v")
+
+let num2VVar = document.getElementById("num2v")
+
+let num3VVar = document.getElementById("num3v")
+
+let num4VVar = document.getElementById("num4v")
+
+let num5VVar = document.getElementById("num5v")
+
+let num6VVar = document.getElementById("num6v")
+
+let num7VVar = document.getElementById("num7v")
+
+let num8VVar = document.getElementById("num8v")
+
+let num9VVar = document.getElementById("num9v")
+
+// Dichiarazione variabili 30-39
+let num0TVar = document.getElementById("num0t") 
+
+let num1TVar = document.getElementById("num1t")
+
+let num2TVar = document.getElementById("num2t")
+
+let num3TVar = document.getElementById("num3t")
+
+let num4TVar = document.getElementById("num4t")
+
+let num5TVar = document.getElementById("num5t")
+
+let num6TVar = document.getElementById("num6t")
+
+let num7TVar = document.getElementById("num7t")
+
+let num8TVar = document.getElementById("num8t")
+
+let num9TVar = document.getElementById("num9t")
+
+// Dichiarazione variabili 40-49
+let num0QVar = document.getElementById("num0q") 
+
+let num1QVar = document.getElementById("num1q")
+
+let num2QVar = document.getElementById("num2q")
+
+let num3QVar = document.getElementById("num3q")
+
+let num4QVar = document.getElementById("num4q")
+
+let num5QVar = document.getElementById("num5q")
+
+let num6QVar = document.getElementById("num6q")
+
+let num7QVar = document.getElementById("num7q")
+
+let num8QVar = document.getElementById("num8q")
+
+let num9QVar = document.getElementById("num9q")
+
+// Dichiarazione variabili 50-59
+let num0CVar = document.getElementById("num0c") 
+
+let num1CVar = document.getElementById("num1c")
+
+let num2CVar = document.getElementById("num2c")
+
+let num3CVar = document.getElementById("num3c")
+
+let num4CVar = document.getElementById("num4c")
+
+let num5CVar = document.getElementById("num5c")
+
+let num6CVar = document.getElementById("num6c")
+
+let num7CVar = document.getElementById("num7c")
+
+let num8CVar = document.getElementById("num8c")
+
+let num9CVar = document.getElementById("num9c")
+
+
+// Dichiarazione variabili 60-69
+let num0SVar = document.getElementById("num0s") 
+
+let num1SVar = document.getElementById("num1s")
+
+let num2SVar = document.getElementById("num2s")
+
+let num3SVar = document.getElementById("num3s")
+
+let num4SVar = document.getElementById("num4s")
+
+let num5SVar = document.getElementById("num5s")
+
+let num6SVar = document.getElementById("num6s")
+
+let num7SVar = document.getElementById("num7s")
+
+let num8SVar = document.getElementById("num8s")
+
+let num9SVar = document.getElementById("num9s")
+
+
+// Dichiarazione variabili 70-79
+let num0SSVar = document.getElementById("num0ss") 
+
+let num1SSVar = document.getElementById("num1ss")
+
+let num2SSVar = document.getElementById("num2ss")
+
+let num3SSVar = document.getElementById("num3ss")
+
+let num4SSVar = document.getElementById("num4ss")
+
+let num5SSVar = document.getElementById("num5ss")
+
+let num6SSVar = document.getElementById("num6ss")
+
+let num7SSVar = document.getElementById("num7ss")
+
+let num8SSVar = document.getElementById("num8ss")
+
+let num9SSVar = document.getElementById("num9ss")
+
+
+// Dichiarazione variabili 80-89
+let num0OVar = document.getElementById("num0o") 
+
+let num1OVar = document.getElementById("num1o")
+
+let num2OVar = document.getElementById("num2o")
+
+let num3OVar = document.getElementById("num3o")
+
+let num4OVar = document.getElementById("num4o")
+
+let num5OVar = document.getElementById("num5o")
+
+let num6OVar = document.getElementById("num6o")
+
+let num7OVar = document.getElementById("num7o")
+
+let num8OVar = document.getElementById("num8o")
+
+let num9OVar = document.getElementById("num9o")
+
+
+// Dichiarazione variabili 90-99
+let num0NVar = document.getElementById("num0n") 
+
+let num1NVar = document.getElementById("num1n")
+
+let num2NVar = document.getElementById("num2n")
+
+let num3NVar = document.getElementById("num3n")
+
+let num4NVar = document.getElementById("num4n")
+
+let num5NVar = document.getElementById("num5n")
+
+let num6NVar = document.getElementById("num6n")
+
+let num7NVar = document.getElementById("num7n")
+
+let num8NVar = document.getElementById("num8n")
+
+let num9NVar = document.getElementById("num9n")
+
+
+// Dichiarazione variabili 100
+let num0CCVar = document.getElementById("num0cc") 
+
+
+// Ciclo For per l'inserimento dei numeri da 1 a 10 nel codice HTML
+    for (i = 0; i < 101; i++) {
+        switch(i){
+            case 0:
+            num0Var.innerHTML = "Buzz";
+            case 1:
+            num1Var.innerHTML = i;
+            case 2:
+            num2Var.innerHTML = i;    
+            case 3:
+            num3Var.innerHTML = "Fizz";    
+            case 4:
+            num4Var.innerHTML = i;    
+            case 5:
+            num5Var.innerHTML = "Buzz";    
+            case 6:
+            num6Var.innerHTML = "Fizz";    
+            case 7:
+            num7Var.innerHTML = i;    
+            case 8:
+            num8Var.innerHTML = i;    
+            case 9:
+            num9Var.innerHTML = "Fizz";
+
+            case 10:
+            num0DVar.innerHTML = "Buzz";
+            case 11:
+            num1DVar.innerHTML = i;
+            case 12:
+            num2DVar.innerHTML = "Fizz";    
+            case 13:
+            num3DVar.innerHTML = i;    
+            case 14:
+            num4DVar.innerHTML = i;    
+            case 15:
+            num5DVar.innerHTML = "FizzBuzz";    
+            case 16:
+            num6DVar.innerHTML = i;    
+            case 17:
+            num7DVar.innerHTML = i;    
+            case 18:
+            num8DVar.innerHTML = "Fizz";    
+            case 19:
+            num9DVar.innerHTML = i;
+            
+            case 20:
+            num0VVar.innerHTML = "Buzz";
+            case 21:
+            num1VVar.innerHTML = "Fizz";
+            case 22:
+            num2VVar.innerHTML = i;    
+            case 23:
+            num3VVar.innerHTML = i;    
+            case 24:
+            num4VVar.innerHTML = "Fizz";    
+            case 25:
+            num5VVar.innerHTML = "Buzz";    
+            case 26:
+            num6VVar.innerHTML = i;    
+            case 27:
+            num7VVar.innerHTML = "Fizz";    
+            case 28:
+            num8VVar.innerHTML = i;    
+            case 29:
+            num9VVar.innerHTML = i; 
+            
+            case 30:
+            num0TVar.innerHTML = "FizzBuzz";
+            case 31:
+            num1TVar.innerHTML = i;
+            case 32:
+            num2TVar.innerHTML = i;    
+            case 33:
+            num3TVar.innerHTML = "Fizz";    
+            case 34:
+            num4TVar.innerHTML = i;    
+            case 35:
+            num5TVar.innerHTML = "Buzz";    
+            case 36:
+            num6TVar.innerHTML = "Fizz";    
+            case 37:
+            num7TVar.innerHTML = i;    
+            case 38:
+            num8TVar.innerHTML = i;    
+            case 39:
+            num9TVar.innerHTML = "Fizz"; 
+
+            case 40:
+            num0QVar.innerHTML = "Buzz";
+            case 41:
+            num1QVar.innerHTML = i;
+            case 42:
+            num2QVar.innerHTML = "Fizz";    
+            case 43:
+            num3QVar.innerHTML = i;    
+            case 44:
+            num4QVar.innerHTML = i;    
+            case 45:
+            num5QVar.innerHTML = "FizzBuzz";    
+            case 46:
+            num6QVar.innerHTML = i;    
+            case 47:
+            num7QVar.innerHTML = i;    
+            case 48:
+            num8QVar.innerHTML = "Fizz";    
+            case 49:
+            num9QVar.innerHTML = i; 
+
+            case 50:
+            num0CVar.innerHTML = "Buzz";
+            case 51:
+            num1CVar.innerHTML = "Fizz";
+            case 52:
+            num2CVar.innerHTML = i;    
+            case 53:
+            num3CVar.innerHTML = i;    
+            case 54:
+            num4CVar.innerHTML = "Fizz";    
+            case 55:
+            num5CVar.innerHTML = "Buzz";    
+            case 56:
+            num6CVar.innerHTML = i;    
+            case 57:
+            num7CVar.innerHTML = "Fizz";    
+            case 58:
+            num8CVar.innerHTML = i;    
+            case 59:
+            num9CVar.innerHTML = i; 
+
+            case 60:
+            num0SVar.innerHTML = "FizzBuzz";
+            case 61:
+            num1SVar.innerHTML = i;
+            case 62:
+            num2SVar.innerHTML = i;    
+            case 63:
+            num3SVar.innerHTML = "Fizz";    
+            case 64:
+            num4SVar.innerHTML = i;    
+            case 65:
+            num5SVar.innerHTML = "Buzz";    
+            case 66:
+            num6SVar.innerHTML = "Fizz";    
+            case 67:
+            num7SVar.innerHTML = i;    
+            case 68:
+            num8SVar.innerHTML = i;    
+            case 69:
+            num9SVar.innerHTML = "Fizz"; 
+
+            case 70:
+            num0SSVar.innerHTML = "Buzz";
+            case 71:
+            num1SSVar.innerHTML = i;
+            case 72:
+            num2SSVar.innerHTML = "Fizz";    
+            case 73:
+            num3SSVar.innerHTML = i;    
+            case 74:
+            num4SSVar.innerHTML = i;    
+            case 75:
+            num5SSVar.innerHTML = "FizzBuzz";    
+            case 76:
+            num6SSVar.innerHTML = i;    
+            case 77:
+            num7SSVar.innerHTML = i;    
+            case 78:
+            num8SSVar.innerHTML = "Fizz";    
+            case 79:
+            num9SSVar.innerHTML = i; 
+
+            case 80:
+            num0OVar.innerHTML = "Buzz";
+            case 81:
+            num1OVar.innerHTML = "Fizz";
+            case 82:
+            num2OVar.innerHTML = i;    
+            case 83:
+            num3OVar.innerHTML = i;    
+            case 84:
+            num4OVar.innerHTML = "Fizz";    
+            case 85:
+            num5OVar.innerHTML = "Buzz";    
+            case 86:
+            num6OVar.innerHTML = i;    
+            case 87:
+            num7OVar.innerHTML = "Fizz";    
+            case 88:
+            num8OVar.innerHTML = i;    
+            case 89:
+            num9OVar.innerHTML = i; 
+
+            case 90:
+            num0NVar.innerHTML = "FizzBuzz";
+            case 91:
+            num1NVar.innerHTML = i;
+            case 92:
+            num2NVar.innerHTML = i;    
+            case 93:
+            num3NVar.innerHTML = "Fizz";    
+            case 94:
+            num4NVar.innerHTML = i;    
+            case 95:
+            num5NVar.innerHTML = "Buzz";    
+            case 96:
+            num6NVar.innerHTML = "Fizz";    
+            case 97:
+            num7NVar.innerHTML = i;    
+            case 98:
+            num8NVar.innerHTML = i;    
+            case 99:
+            num9NVar.innerHTML = "Fizz"; 
+
+            case 100:
+            num0CCVar.innerHTML = "Buzz";
+           
+        }
+        
+    }
+
+  
+  
+    
+
